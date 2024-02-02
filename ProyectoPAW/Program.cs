614beb0f8c09e563055bc20e6705ced871e29c60
jgarcia60436 <78770752+jgarcia60436@users.noreply.github.com>
2024-02-02 14:33:28 -0600

    Agregar js, arreglar referencias y carrusel

--- ProyectoPAW/Program.cs
+++ ProyectoPAW/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Inicio}/{id?}");
+    pattern: "{controller=Home}/{action=Ingresar}/{id?}");
 
 app.Run();
---
+++ ProyectoPAW/wwwroot/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+    
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+});
+
